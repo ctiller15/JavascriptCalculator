c34f0b0621b4e8e713aa137da843cf7151d2e413
ctiller15 <christiller2015@gmail.com>
2017-07-25 01:36:13 -0400

    Add number creation. Chain clicked numbers.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
+var display = document.querySelector(".display")
 var calc = document.querySelector("div");
 
 for(var i = 0; i < 9; i++){
 	let btn = document.createElement("button");
-	btn.classList.add("button" + i + 1);
+	let val = i + 1;
+	console.log(val);
+	btn.classList.add("button_" + val);
+	btn.textContent = val;
+	btn.addEventListener("click", function(){
+		console.log(val);
+		display.textContent += val;
+	});
 	calc.appendChild(btn);
 }

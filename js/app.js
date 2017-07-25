51dd902639e7b5b5c5ca1ee008de1527c89ed443
ctiller15 <christiller2015@gmail.com>
2017-07-25 17:58:40 -0400

    Remove event listeners upon completion and allow chained addition.

--- js/app.js
+++ js/app.js
@@ -1,34 +1,49 @@
 var display = document.querySelector(".display")
 var calc = document.querySelector("div");
+var firstVal = "";
+var secondVal = "";
+var option = 0;
 
-function equality(first, second, op){
+
+function equality(){
 	// setting up all of the operations. 1 is addition
-	if(op === 1){
-		sum = first + second;
+	if(option === 1){
+		sum = Number(firstVal) + Number(secondVal);
 		display.textContent = sum;
-		console.log(sum);
-		return sum;
+		firstVal = "";
+		secondVal = "";
 	}
 	// 2 is subtraction.
 	// 3 is multiplication.
 	// 4 is division.
+	// resetting all of the event listeners on the buttons.
+	nums.forEach(function(num){
+		num.removeEventListener("click", updateNum, false);
+	});
+	// reset the option.
+	option = 0;
+}
+
+function updateNum(){
+	secondVal += this.textContent;
+	console.log(secondVal);
+	console.log("I will be here until the event is removed!");
 }
 
 // Creating all numerical buttons.
 for(var i = 0; i < 9; i++){
 	let btn = document.createElement("button");
 	let val = i + 1;
-	console.log(val);
 	btn.classList.add("button_" + val);
 	btn.classList.add("number");
 	btn.textContent = val;
 	btn.addEventListener("click", function(){
-		console.log(val);
 		display.textContent += val;
 	});
 	calc.appendChild(btn);
 }
 
+// Selecting the entire set of numbers.
 var nums = document.querySelectorAll(".number");
 
 
@@ -38,27 +53,19 @@ plus.textContent = "+";
 var equals = document.createElement("button");
 equals.classList.add("equal");
 equals.textContent = "=";
-console.log(equals);
 
 plus.addEventListener("click", function(){
+	equality();
+	option = 1;
 	firstVal = display.textContent;
-	console.log(firstVal);
 	// clearing the display.
 	display.textContent = "";
-	var combinedVal = "";
+	secondVal = "";
 	nums.forEach(function(num){
-		num.addEventListener("click", function(){
-			combinedVal += this.textContent;
-			console.log("adding " + combinedVal);
-		});
+		num.addEventListener("click", updateNum, false);
 	});
 	// the first parameter is always the event e.
-	equals.addEventListener("click", function(){
-		equality(Number(firstVal), Number(combinedVal), 1);
-		// clearing the display, as well as all of the values.
-		firstVal = "";
-		combinedVal = "";
-	});
+	equals.addEventListener("click", equality, false);
 });
 
 calc.appendChild(plus);
